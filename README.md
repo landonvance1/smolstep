ef63fa98fb13704ccd3976c184b458636be6dfb4
Landon Vance <96551356+landonpvance@users.noreply.github.com>
2022-06-11 17:45:19 -0500

    Added top bar to the app. Also included font-awesome in the project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# smolstep
-Smolstep
+# Initialization
+
+npm install -g @angular/cli
+npm install @fortawesome/fontawesome-svg-core
+npm install @fortawesome/free-brands-svg-icons
+npm install @fortawesome/free-regular-svg-icons
+npm install @fortawesome/free-solid-svg-icons
+npm install @fortawesome/angular-fontawesome
--- smolstep/src/app/app.module.ts
+++ smolstep/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppTopBarComponent } from './app-top-bar/app-top-bar.component';
+
+import { FontAwesomeModule, FaIconLibrary  } from '@fortawesome/angular-fontawesome';
+import { faSpinner, faBars } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faSpinner, faBars);
+  }
+}
