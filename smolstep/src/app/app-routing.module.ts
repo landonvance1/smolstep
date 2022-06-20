07eb87db2e741ba0aef738c9c9ce209e3fa29274
landonvance1 <landonpvance@gmail.com>
2022-06-20 18:56:03 -0500

    added do task page and routing

--- smolstep/src/app/app-routing.module.ts
+++ smolstep/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoTaskPageComponent } from './do-task-page/do-task-page.component';
+import { LandingContentComponent } from './landing-content/landing-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'doTask', component: DoTaskPageComponent },
+  { path: '', component: LandingContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- smolstep/src/app/app.module.ts
+++ smolstep/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppTopBarComponent } from './app-top-bar/app-top-bar.component';
 import { FontAwesomeModule, FaIconLibrary  } from '@fortawesome/angular-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { LandingContentComponent } from './landing-content/landing-content.component';
+import { DoTaskPageComponent } from './do-task-page/do-task-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppTopBarComponent,
-    LandingContentComponent
+    LandingContentComponent,
+    DoTaskPageComponent
   ],
   imports: [
     BrowserModule,
